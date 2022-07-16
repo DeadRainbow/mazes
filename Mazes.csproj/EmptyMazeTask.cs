8ec59aca09cd2ea621c1f54804206c1d5b4fbee8
DeadRainbow <109438272+DeadRainbow@users.noreply.github.com>
2022-07-16 22:25:00 +0200

    Add files via upload

---
+++ Mazes.csproj/EmptyMazeTask.cs
@@ -0,0 +1,21 @@
+﻿namespace Mazes
+{
+	public static class EmptyMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+			for (int i = 0; i < width + height; i++)
+            {
+				if (!robot.Finished && robot.Y != height - 2)
+				{
+					robot.MoveTo(Direction.Down);
+				}
+
+				else if (!robot.Finished && robot.X != width - 2)
+				{
+					robot.MoveTo(Direction.Right);
+				}
+			}
+		}	
+	}
+}
---
+++ Mazes.csproj/SnakeMazeTask.cs
@@ -0,0 +1,48 @@
+﻿namespace Mazes
+{
+	public static class SnakeMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+			for (int i = 1; i < (width * 8) + height; i++)
+			{
+				MoveToRightWall(robot, width, height);
+
+				MoveToDownWall(robot, width, height);
+
+				MoveToDownWall2(robot, width, height);
+
+				MoveToLeftWall(robot, width, height);
+			}
+		}
+		public static void MoveToRightWall(Robot robot, int width, int height)
+        {
+			if ((!robot.Finished && robot.X != width - 2) && (!robot.Finished && robot.Y == 13 || robot.Y == 9 || robot.Y == 5 || robot.Y == 1))
+			{
+				robot.MoveTo(Direction.Right);
+			}
+		}
+		public static void MoveToDownWall(Robot robot, int width, int height)
+        {
+			if (!robot.Finished && ((robot.Y >= 1 && robot.Y < 3) || (robot.Y >= 5 && robot.Y < 7) || (robot.Y >= 9 && robot.Y < 11) || (robot.Y >= 13 && robot.Y < 15)) && (robot.X == width - 2))
+			{
+				robot.MoveTo(Direction.Down);
+			}
+		}
+		public static void MoveToDownWall2(Robot robot, int width, int height)
+		{
+			if (!robot.Finished && ((robot.Y >= 3 && robot.Y < 5) || (robot.Y >= 7 && robot.Y < 9) || (robot.Y >= 11 && robot.Y < 13)) && (robot.X == 1))
+			{
+				robot.MoveTo(Direction.Down);
+			}
+		}
+
+		public static void MoveToLeftWall(Robot robot, int width, int height)
+		{
+			if ((!robot.Finished && robot.X != 1) && (!robot.Finished && robot.Y == 15 || robot.Y == 11 || robot.Y == 7 || robot.Y == 3))
+			{
+				robot.MoveTo(Direction.Left);
+			}
+		}
+	}
+}
